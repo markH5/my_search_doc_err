51ea61ba48556282db66865ee222568964b0b8c1
markH5 <chenmarkh@gmail.com>
2025-02-06 17:28:36 +0800

    minimal reproduction

--- theme/index.tsx
+++ theme/index.tsx
@@ -1,26 +1,3 @@
-# Rspress website
-
-## Setup 1
-
-Install the dependencies:
-
-```bash
-pnpm create rspress@latest
-pnpm i react
-pnpm i react-dom
-
-pnpm i typescript -D
-pnpm i @types/react -D
-pnpm i @types/react-dom -D
-
-```
-
-## Setup 2
-
-copy code from doc <https://rspress.dev/guide/advanced/custom-theme#3-custom-home-page-and-404-page>
-
-```tsx
-// theme/index.tsx
 import Theme, { Search } from 'rspress/theme';
 
 // Use slot
@@ -42,8 +19,3 @@ const MySearch = () => <div className="my-search">{Search}</div>;
 export { MySearch as Search };
 // re-export
 export * from 'rspress/theme';
-```
-
-## type error
-
-hover at `{Search}` , get type error
